7b917d3656ac1ece6366f4b15d97445629e0611a
Danilo <danilo_srr@hotmail.com>
2022-07-27 19:26:46 -0300

    faker, cypress

--- front-end/cypress/e2e/Recommendation.cy.js
+++ front-end/cypress/e2e/Recommendation.cy.js
@@ -1,3 +1,5 @@
+import { faker } from "@faker-js/faker";
+
 beforeEach(() => {
 	cy.cleardb();
 });
@@ -6,8 +8,8 @@ describe("create recommendation", () => {
 	it("new recommendation should appear", () => {
 		cy.visit("http://localhost:3000/");
 
-		cy.get('input[placeholder="Name"]').type("Recommendation");
-		cy.get('input[placeholder="https://youtu.be/..."]').type("https://www.youtube.com/watch?v=pjX3J9EpAr0");
+		cy.get('input[placeholder="Name"]').type(`${faker.random.word()}`);
+		cy.get('input[placeholder="https://youtu.be/..."]').type(`https://www.youtube.com/watch?v=${faker.random.alphaNumeric(10)}`);
 		cy.get("button").click();
 		cy.get('article').should('be.visible');
 	});
