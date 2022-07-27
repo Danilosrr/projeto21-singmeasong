863ca847815a8c4eaf2afd0860678a7465728ded
Danilo <danilo_srr@hotmail.com>
2022-07-27 16:36:20 -0300

    upvote and downvote route test

--- back-end/tests/factories/recommendationFactory.ts
+++ back-end/tests/factories/recommendationFactory.ts
@@ -1,5 +1,6 @@
 import { faker } from "@faker-js/faker";
-import { CreateRecommendationData } from "../../src/services/recommendationsService";
+import { prisma } from "../../src/database.js";
+import { CreateRecommendationData } from "../../src/services/recommendationsService.js";
 
 function recommendationBody(){
 
@@ -21,7 +22,33 @@ function recommendationBodyWrong(){
     return recommendation;
 };
 
+async function getRecommendation(name:string){
+    return await prisma.recommendation.findUnique({
+        where: { name }
+    })
+}
+
+async function createRecommendation(){
+    const body = recommendationBody(); 
+
+    return await prisma.recommendation.create({
+        data: body,
+      });
+};
+
+async function setRecommendationScore(id:number,score:number) {
+    return await prisma.recommendation.update({
+        where: { id },
+        data: {
+          score:  score ,
+        }
+    });
+}
+
 export const recommendationFactory = {
     recommendationBody,
-    recommendationBodyWrong
+    recommendationBodyWrong,
+    getRecommendation,
+    createRecommendation,
+    setRecommendationScore
 }
--- back-end/tests/recommendation.test.ts
+++ back-end/tests/recommendation.test.ts
@@ -25,3 +25,51 @@ describe("POST /recommendations", () => {
         expect(response.status).toBe(422);
     });
 });
+
+describe("POST /recommendations/:id/upvote", () => {  
+
+    it("should return a status 200", async () => {
+        const recommendation = await recommendationFactory.createRecommendation();
+
+        const response = await supertest(app).post(`/recommendations/${+recommendation.id}/upvote`);
+        expect(response.status).toBe(200);
+    });
+
+    it("should return a status 200", async () => {
+        const recommendation = await recommendationFactory.createRecommendation();
+
+        const upvote = await supertest(app).post(`/recommendations/${+recommendation.id}/upvote`);
+        const response = await recommendationFactory.getRecommendation(recommendation.name);
+        console.log(response)
+        expect(response.score).toBe(1);
+    });
+});
+
+describe("POST /recommendations/:id/downvote", () => {  
+    
+    it("should return a status 200", async () => {
+        const recommendation = await recommendationFactory.createRecommendation();
+
+        const response = await supertest(app).post(`/recommendations/${+recommendation.id}/downvote`);
+        expect(response.status).toBe(200);
+    });
+
+    it("should return a status 200", async () => {
+        const recommendation = await recommendationFactory.createRecommendation();
+
+        const downvote = await supertest(app).post(`/recommendations/${+recommendation.id}/downvote`);
+        const response = await recommendationFactory.getRecommendation(recommendation.name);
+        expect(response.score).toBe(-1);
+    });
+
+    it("should delete recommendation when score < -5", async () => {
+        const recommendation = await recommendationFactory.createRecommendation();
+
+        const setDownVotes = await recommendationFactory.setRecommendationScore(recommendation.id,-5);
+        const downvote = await supertest(app).post(`/recommendations/${+recommendation.id}/downvote`);
+
+        const checkDatabase = await recommendationFactory.getRecommendation(recommendation.name);
+        console.log(checkDatabase)
+        expect(checkDatabase).toBe(null);
+    });
+});
