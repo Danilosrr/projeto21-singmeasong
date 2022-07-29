1144bd196ff28beb069e52f6d99366b992d7dbfd
Danilo <danilo_srr@hotmail.com>
2022-07-28 21:29:43 -0300

    changes in gitignore, added function to unitary test

--- back-end/tests/unit/recommendationsService.test.ts
+++ back-end/tests/unit/recommendationsService.test.ts
@@ -5,10 +5,20 @@ import { recommendationService } from "../../src/services/recommendationsService
 
 
 describe("get", () => {
-    it("expect to return user", async () => {        
+    it("expect to call function", async () => {        
         const findAll = jest.spyOn(recommendationRepository,"findAll").mockImplementationOnce(():any=>{})
 
         await recommendationService.get();
         expect(findAll).toHaveBeenCalled();
+    });
+});
+
+describe("getTop", () => {
+    it("expect to call function", async () => {    
+        const amount = 9    
+        const getAmountByScore = jest.spyOn(recommendationRepository,"getAmountByScore").mockImplementationOnce(():any=>{})
+
+        await recommendationService.getTop(amount);
+        expect(getAmountByScore).toHaveBeenCalled();
     });
 });
